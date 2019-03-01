bdd10a1f7a8b281d8849a1924fdfbf35f5e37f21
Denis Screamer <deni.screamer@gmail.com>
2019-03-01 17:53:09 +0300

    add new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './main/main.module#MainModule' },
-  { path: 'blank', loadChildren: './blank/blank.module#BlankModule' }
+  { path: 'blank', loadChildren: './blank/blank.module#BlankModule' },
+  { path: 'анкета', redirectTo: '/blank', pathMatch: 'full' },
+  { path: 'предназначение', redirectTo: '/blank/1', pathMatch: 'full' },
+  { path: 'взаимоотношения', redirectTo: '/blank/2', pathMatch: 'full' },
+  { path: 'детская', redirectTo: '/blank/3', pathMatch: 'full' }
 ];
 
 @NgModule({
